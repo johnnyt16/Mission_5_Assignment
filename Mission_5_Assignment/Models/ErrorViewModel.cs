60fc42cae4c07e17d4db58d956580ed7540627f5
johnnyt16 <johntaylormcc@gmail.com>
2025-02-05 14:22:23 -0700

    Initial commit

---
+++ Mission_5_Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission_5_Assignment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
